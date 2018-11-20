91352c6e7e39faceed2ebcb22fd85efaa0fa5b6c
swmcdona <swmcdona@ssck10556-x01.stat.sfu.ca>
2018-11-20 13:22:01 -0800

    Uploading banana code

--- One-shot/SymbolicTest.m
+++ One-shot/SymbolicTest.m
@@ -1,29 +1,34 @@
-syms t epsilon real
-%Gamma parameters
-a = 2; b = 4;
+syms t epsilon b a real
+assumeAlso(epsilon > 0)
+%assume(delta > epsilon)
+assumeAlso(a>1); assumeAlso(b>0);
+%assumeAlso(lambda > 0);
+%b = 3;
 mle = (a-1)/b; mu = a/b; sd = sqrt(a)/b;
 
+lambda = 1.5*epsilon*sd;
+
 %Gamma pdf
-f(t) = (b^a)*t^(a-1)*exp(-b*t)/gamma(a);
-
+f(t) = piecewise(t>0,(b^a)*t^(a-1)*exp(-b*t)/gamma(a),0);
 %Hyperparameters and endpoints for GP solver
-%Make sure that the left one doesn't go beyond zero
-alpha = 1; lambda = 1;
+%Make sure that the left onef doesn't go beyond zero
+alpha = 1; %lambda = 1;
 %delta = 2*epsilon;
-s1 = max(mle-2*epsilon*sd, 0); s2 = max(mle-epsilon*sd,0);
-s4 = mle+epsilon*sd; s5 = mle+2*epsilon*sd;
-
+%s1 = piecewise(epsilon<sqrt(a)-1/sqrt(a),mle-epsilon*sd,0);
+%s1 = 0;
+s1 = mle-epsilon*sd;
+s3 = mle+epsilon*sd;
 %Prior mean function:
 m_t(t) = f(mle)*exp(0.5*subs(diff(log(f),2),mle)*(t-mle)^2);
 
 %Interrogation points
-s = [s1 s2 mle s4 s5]';
+s = [s1 mle s3]';
 
 %Here we go
-correction(epsilon) = RQ1d_se(s5,s,lambda,s1,s5)*inv(RR1d_se(s,s,lambda,s1,s5))*RQ1d_se(s,s5,lambda,s1,s5);
-variance(epsilon) = simplify(QQ1d_se(s5,s5,lambda,s1,s5) - correction(epsilon));
+correction(epsilon, a) = RQ1d_se(s3,s,lambda,s1,s3)*inv(RR1d_se(s,s,lambda,s1,s3))*RQ1d_se(s,s3,lambda,s1,s3);
+variance(epsilon, a) = simplify(QQ1d_se(s3,s3,lambda,s1,s3) - correction(epsilon, a));
+bias(epsilon, a) = simplify(RQ1d_se(s3,s,lambda,s1,s3)*inv(RR1d_se(s,s,lambda,s1,s3))*(f(s)-m_t(s)));
 
-bias(epsilon) = simplify((RQ1d_se(s5,s,lambda,s1,s5)*inv(RR1d_se(s,s ...
-    ,lambda,s1,s5))*(f(s)-m_t(s))))^2;
+KL(epsilon, a) = 0.5*alpha*bias^2/variance
 
-KL(epsilon) = 0.5*alpha*bias(epsilon)/variance(epsilon);
+
