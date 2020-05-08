cfaa0ddef21126e4bcc1b4ac9381d9cd93be0f40
shaun-mcdonald <swmcdona@sfu.ca>
2020-05-08 13:03:39 -0500

    Cleaning out the crap in One-shot

--- One-shot/SymbolicTest.m
+++ One-shot/SymbolicTest.m
@@ -1,34 +1,46 @@
+%% SymbolicTest.m
+% Just something I used in the experiments for my JSM 2018 poster. Using
+% symbolic math to try and optimize the diagnostic in 1-D using 3
+% interrogation points. The assumption is you'd want high KL divergence
+% from the "null" (i.e. low variance, but bring it as far away as possible
+% from the prior mean). For Gaussians of equal variance, KL is simply
+% bias^2/(2*variance)
+
 syms t epsilon b a real
-assumeAlso(epsilon > 0)
-%assume(delta > epsilon)
-assumeAlso(a>1); assumeAlso(b>0);
-%assumeAlso(lambda > 0);
-%b = 3;
+% t = argument; a, b = gamma pdf parameters; epsilon = spacing of
+% interrogation points in units of standard deviation
+assumeAlso(epsilon > 0 & a > 1 & b > 0);
 mle = (a-1)/b; mu = a/b; sd = sqrt(a)/b;
 
+% Taking covariance hyperparameters to be functions of epsilon as in
+% Charlie's thesis
 lambda = 1.5*epsilon*sd;
+alpha = 1/(epsilon*sd);
 
-%Gamma pdf
+% Gamma pdf full function
 f(t) = piecewise(t>0,(b^a)*t^(a-1)*exp(-b*t)/gamma(a),0);
-%Hyperparameters and endpoints for GP solver
-%Make sure that the left onef doesn't go beyond zero
-alpha = 1; %lambda = 1;
-%delta = 2*epsilon;
-%s1 = piecewise(epsilon<sqrt(a)-1/sqrt(a),mle-epsilon*sd,0);
-%s1 = 0;
+
+% Hyperparameters and endpoints for GP solver
 s1 = mle-epsilon*sd;
+% Can force s1 not to go lower than 0 by uncommenting this line 
+% s1 = piecewise(epsilon<sqrt(a)-1/sqrt(a),mle-epsilon*sd,0);
 s3 = mle+epsilon*sd;
-%Prior mean function:
+
+%Prior mean function
 m_t(t) = f(mle)*exp(0.5*subs(diff(log(f),2),mle)*(t-mle)^2);
 
 %Interrogation points
 s = [s1 mle s3]';
 
-%Here we go
-correction(epsilon, a) = RQ1d_se(s3,s,lambda,s1,s3)*inv(RR1d_se(s,s,lambda,s1,s3))*RQ1d_se(s,s3,lambda,s1,s3);
-variance(epsilon, a) = simplify(QQ1d_se(s3,s3,lambda,s1,s3) - correction(epsilon, a));
-bias(epsilon, a) = simplify(RQ1d_se(s3,s,lambda,s1,s3)*inv(RR1d_se(s,s,lambda,s1,s3))*(f(s)-m_t(s)));
+% Here we go
+correction(epsilon, a) = RQ1d_se(s3,s,lambda,s1,s3)*...
+    inv(RR1d_se(s,s,lambda,s1,s3))*RQ1d_se(s,s3,lambda,s1,s3);
+variance(epsilon, a) = simplify(QQ1d_se(s3,s3,lambda,s1,s3) -...
+    correction(epsilon, a));
+bias(epsilon, a) = simplify(RQ1d_se(s3,s,lambda,s1,s3)*...
+    inv(RR1d_se(s,s,lambda,s1,s3))*(f(s)-m_t(s)));
 
+% KL is ultimately a function of epsilon and gamma shape. The scale
+% parameter more or less controls scaling, and therefore has no effect on
+% the optimal epsilon
 KL(epsilon, a) = 0.5*alpha*bias^2/variance
-
-
--- One-shot/onedimnormal.m
+++
@@ -1,8 +0,0 @@
-function [f, mle] = onedimnormal(theta)
-
-f = @(t) normpdf(t,theta(1), theta(2));
-mle = theta(1);
-
-
-
-
