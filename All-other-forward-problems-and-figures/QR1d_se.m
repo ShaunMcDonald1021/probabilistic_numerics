6d8fc98f968cf2cd5c206fdbb748d4198cf39a2b
shaun-mcdonald <swmcdona@sfu.ca>
2020-05-08 10:51:26 -0500

    Restructuring uqdes

--- All-other-forward-problems-and-figures/QR1d_se.m
+++
@@ -1,6 +0,0 @@
-function qrt = QR1d_se(u1,v1,w,a,b)
-
-qrt =  RQ1d_se(v1,u1,w,a,b)';
-
-
-end
--- JAKSTAT-inverse-problem-example/prior_normal.m
+++
@@ -1,4 +0,0 @@
-function p_ab = prior_normal(x,mu,var)
-%   Setting up the prior for a,b as independent normals
-
-p_ab = normpdf(x,mu,sqrt(var));
