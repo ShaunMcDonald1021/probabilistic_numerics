67251a316d025b561ec222c5ff6fe093fa2e46c0
shaun-mcdonald <swmcdona@sfu.ca>
2020-05-08 13:03:39 -0500

    Cleaning out the crap in One-shot

--- One-shot/PowerCalc.m
+++ One-shot/PowerCalc.m
@@ -1,29 +1,41 @@
+%% PowerCalc.m
+% Another script I made to play around with symbolic math for the
+% experiments for my JSM 2018 presentation. Using the diagnostic in 1-D
+% with 3 interrogation points, we want to optimize interrogation spacing
+% w.r.t. KL divergence and/or test power. These two should be equivalent,
+% and we can investigate that with this script.
+
+% If you want to record values for power in an array at varying parameter
+% values. Presumably you'd fill this in with a loop, but for some reason
+% 2018 Shaun didn't put that in this script
 skew_vec = 0.25:0.05:1.75;
 eps_vec = 0.1:0.05:3;
 b_vec = 10.^(-1:0.5:2); lambda = 2;
-
 power_arr = zeros(length(skew_vec), length(eps_vec), length(b_vec));
 
 syms t epsilon skew b real
-assumeAlso(epsilon > 0)
-%assume(delta > epsilon)
-assumeAlso(skew<2); assumeAlso(b>0); assumeAlso(skew > 0);
-%assumeAlso(lambda > 0);
-%a = 2; b = 4;
+% Define shape parameter in terms of skew for a different interpretation
+% that may be somewhat more useful
 a = 4/skew^2;
+assumeAlso(epsilon > 0 & skew < 2 & b> 0 & skew > 0);
+
 mle = (a-1)/b; mu = a/b; sd = sqrt(a)/b;
 
-%Gamma pdf
-f(t) = (b^a)*t^(a-1)*exp(-b*t)/gamma(a);
-%Hyperparameters and endpoints for GP solver
-%Make sure that the left onef doesn't go beyond zero
-alpha = 1; %lambda = 1;
-%delta = 2*epsilon;
-s1 = piecewise(epsilon < sqrt(a)-1/sqrt(a),mle-epsilon*sd, 0); 
-%s1 = 0;
-%s1 = mle-epsilon*sd;
+% Taking covariance hyperparameters to be functions of epsilon as in
+% Charlie's thesis
+lambda = 1.5*epsilon*sd;
+alpha = 1/(epsilon*sd);
+
+% Gamma pdf full function
+f(t) = piecewise(t>0,(b^a)*t^(a-1)*exp(-b*t)/gamma(a),0);
+
+% Hyperparameters and endpoints for GP solver
+s1 = mle-epsilon*sd;
+% Can force s1 not to go lower than 0 by uncommenting this line 
+% s1 = piecewise(epsilon<sqrt(a)-1/sqrt(a),mle-epsilon*sd,0);
 s3 = mle+epsilon*sd;
-%Prior mean function:
+
+%Prior mean function
 m_t(t) = f(mle)*exp(0.5*subs(diff(log(f),2),mle)*(t-mle)^2);
 
 %Interrogation points
@@ -37,14 +49,6 @@ bias(epsilon, skew, b) = simplify((RQ1d_se(s3,s,lambda,s1,s3)*inv(RR1d_se(s,s ..
 
 KL(epsilon, skew, b) = 0.5*alpha*bias(epsilon,skew, b)/variance(epsilon,skew, b);
 
-power = simplify(1+(erf((-1.96-bias/variance)/sqrt(2)) - erf((1.96-bias/variance)/sqrt(2)))/2);
-
-
-%dKL_eps = diff(KL, epsilon);
-%dKL_lam = diff(KL, lambda);
-
-%fixeps = 3;
-%B(epsilon) = subs(bias, lambda, fixeps);
-%V(epsilon) = subs(variance, lambda, fixeps);
-%K(epsilon) = subs(KL, lambda, fixeps);
-
+% Again, power and KL should be 1-1
+power = simplify(1+(erf((-1.96-bias/variance)/sqrt(2)) - ...
+    erf((1.96-bias/variance)/sqrt(2)))/2);
