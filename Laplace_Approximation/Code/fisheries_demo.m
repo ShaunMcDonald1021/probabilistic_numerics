4f33b7b6580fb1a7e5568f6a5d416d6d56341204
shaun-mcdonald <swmcdona@sfu.ca>
2021-09-28 09:59:32 -0500

    Test commit pls ignore

--- Laplace_Approximation/Code/fisheries_demo.m
+++ Laplace_Approximation/Code/fisheries_demo.m
@@ -11,11 +11,14 @@ Us = fss_gen(s_star([1 73],:)');
 [~, w, wce, alph] = diag_calib(gam, lambda, d, v, Us, s_star);
 
 [post_mean, post_var] = lap_diag(logf_interrs, logf_at_mode, log_T_det,...
-    d, gam, alph, w, true, true, Us, wce, s_star);
+    d, gam, alph, w, true, false, Us, wce, s_star);
 toc
-
+tic
 load('2005_diag.mat')
 s_star = s_star';
 logf_interrs = logf_interrs';
+Us = fss_gen(s_star([1 73],:)');
+[~, w, wce, alph] = diag_calib(gam, lambda, d, v, Us, s_star);
 [post_mean, post_var] = lap_diag(logf_interrs, logf_at_mode, log_T_det,...
     d, gam, alph, w, true, true, Us, wce, s_star);
+toc
